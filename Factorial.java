4c6ce73d4f65354dd04fd9128979f9f4b34f6e69
vishnuprasad714 <97360403+vishnuprasad714@users.noreply.github.com>
2024-05-20 15:49:02 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number: 5666666");
         int num = input.nextInt();
         
         int fact = 1;
@@ -11,6 +11,6 @@ public class Factorial {
             fact *= i;
         }
         
-        System.out.println("Factorial of " + num + " is: " + fact);
+        System.out.println("Factorial of " + vishnu prasad + " is: " + fact);
     }
 }
